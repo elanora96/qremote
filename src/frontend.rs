825bd2c4f826388c4eb8ef83ba0f1880a44615f2
Elanora Manson <git@elanora.lol>
2025-02-15 00:12:10 -0800

    All static files embedded in executable

--- src/frontend.rs
+++ src/frontend.rs
@@ -1,19 +1,60 @@
 use super::key_map;
 use super::HostState;
+use axum::{
+    http::{header, StatusCode, Uri},
+    response::{IntoResponse, Response},
+};
 use handlebars::Handlebars;
+use rust_embed::Embed;
 use serde_json::json;
+use std::str;
 use std::sync::Arc;
 
+#[derive(Embed)]
+#[folder = "assets"]
+struct Asset;
+
+#[derive(Embed)]
+#[folder = "templates"]
+struct Template;
+
+pub struct StaticFile<T>(pub T);
+
+impl<T> IntoResponse for StaticFile<T>
+where
+    T: Into<String>,
+{
+    fn into_response(self) -> Response {
+        let path = self.0.into();
+
+        match Asset::get(path.as_str()) {
+            Some(content) => {
+                let mime = mime_guess::from_path(path).first_or_octet_stream();
+                ([(header::CONTENT_TYPE, mime.as_ref())], content.data).into_response()
+            }
+            None => (StatusCode::NOT_FOUND, "404 Not Found").into_response(),
+        }
+    }
+}
+
+pub async fn static_handler(uri: Uri) -> impl IntoResponse {
+    let path = uri.path().trim_start_matches("/assets/").to_string();
+
+    StaticFile(path)
+}
+
 pub fn build_template() -> Arc<Handlebars<'static>> {
+    let embedded_template = Template::get("index.hbs").unwrap().data.into_owned();
+
     Arc::new({
         let mut hbs = Handlebars::new();
-        hbs.register_template_file("index", "templates/index.hbs")
+        hbs.register_template_string("index", str::from_utf8(&embedded_template).unwrap())
             .unwrap();
         hbs
     })
 }
 
-pub async fn serve_frontend(
+pub async fn ui_handler(
     hs: Arc<HostState>,
     hb: Arc<Handlebars<'static>>,
 ) -> axum::response::Html<String> {
--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,12 @@ use axum::{
 use gethostname::gethostname;
 use local_ip_address::local_ip;
 use qremote::{
-    frontend::{build_template, serve_frontend},
+    frontend::{build_template, static_handler, ui_handler},
     websockets::ws_handler,
     HostState,
 };
 use std::{net::SocketAddr, sync::Arc};
-use tower_http::{
-    services::ServeDir,
-    trace::{DefaultMakeSpan, TraceLayer},
-};
+use tower_http::trace::{DefaultMakeSpan, TraceLayer};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
@@ -42,11 +39,11 @@ async fn main() {
             get({
                 let hs = Arc::clone(&host_state);
                 let hb = Arc::clone(&handlebars);
-                move || serve_frontend(hs, hb)
+                move || ui_handler(hs, hb)
             }),
         )
         .route("/ws", any(ws_handler))
-        .nest_service("/assets", ServeDir::new("assets"))
+        .route("/assets/{*path}", get(static_handler))
         .layer(
             TraceLayer::new_for_http()
                 .make_span_with(DefaultMakeSpan::default().include_headers(true)),
